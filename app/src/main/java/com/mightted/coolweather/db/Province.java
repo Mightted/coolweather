3139237c2d4e3539eadef9a696b5c5e12964620f
Mightted <sosowolf0125@gmail.com>
2017-03-07 15:09:39 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/mightted/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.mightted.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 晓深 on 2017/3/7.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
