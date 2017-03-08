8d939c24f62c0cbf44e2c1bac9690c946fd4e263
Mightted <sosowolf0125@gmail.com>
2017-03-08 17:07:26 +0800

    增加后台自动更新天气功能，修改程序图标和名称

--- app/src/main/java/com/mightted/coolweather/WeatherActivity.java
+++ app/src/main/java/com/mightted/coolweather/WeatherActivity.java
@@ -1,14 +1,19 @@
 package com.mightted.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.Image;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -18,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.mightted.coolweather.gson.Forecast;
 import com.mightted.coolweather.gson.Weather;
+import com.mightted.coolweather.service.AutoUpdateService;
 import com.mightted.coolweather.util.HttpUtil;
 import com.mightted.coolweather.util.LogUtil;
 import com.mightted.coolweather.util.Utility;
@@ -57,6 +63,14 @@ public class WeatherActivity extends AppCompatActivity {
 
     private ImageView  bingPicImg;
 
+    public SwipeRefreshLayout swipeRefresh;
+
+    private String mWeatherId;
+
+    public DrawerLayout drawerLayout;
+
+    private Button navButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,12 +93,18 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton = (Button) findViewById(R.id.nav_button);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather", null);
         if(weatherString != null) {
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            mWeatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         } else {
+            mWeatherId = getIntent().getStringExtra("weather_id");
             LogUtil.d("WeatherActivity","开始联网查找");
             String weatherId = getIntent().getStringExtra("weather_id");
             LogUtil.d("WeatherActivity",weatherId);
@@ -92,16 +112,29 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
+        Glide.with(WeatherActivity.this).load("http://www.tianyaseo.com/bingimg.php").into(bingPicImg);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(mWeatherId);
+            }
+        });
 //        String bingPic = prefs.getString("bing_pic",null);
 //        if(bingPic != null) {
 //            Glide.with(this).load(bingPic).into(bingPicImg);
 //        } else {
 //            loadBingPic();
 //        }
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
     }
 
-    private void requestWeather(final String weatherId) {
+    public void requestWeather(final String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=89163f2578a6495ea79b97022bef66ce";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
@@ -111,8 +144,10 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
+
 
             }
 
@@ -126,10 +161,14 @@ public class WeatherActivity extends AppCompatActivity {
                         if(weather != null && "ok".equals(weather.status)) {
                             SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
                             editor.putString("weather",responseText);
+                            editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startActivity(intent);
                         } else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
             }
